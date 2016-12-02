6286af26b8cd98797ad923b76658028c7a777fea
Khrystarlite <ctaiine.rasengai@gmail.com>
2016-12-02 15:34:11 -0800

    calculated frame rate ratio

--- samples/audio_scratch/librosa_camrec.py
+++ samples/audio_scratch/librosa_camrec.py
@@ -13,10 +13,7 @@ y, sr = librosa.load(filename)
 # 3. Run the default beat tracker
 tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 
-print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+# tmp = librosa.decompose()
 
-# 4. Convert the frame indices of beat events into timestamps
-beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 
-print('Saving output to tmp.csv')
-librosa.output.times_csv('out_Data/tmp.csv', beat_times)
+
