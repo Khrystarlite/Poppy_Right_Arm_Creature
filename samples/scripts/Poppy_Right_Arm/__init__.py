4767375b961654adaef851145c21a85991467cfa
yehsk8rz <fyeh@ucmerced.edu>
2017-01-24 14:36:31 -0800

    prog

--- samples/scripts/Poppy_Right_Arm/__init__.py
+++
@@ -1,3 +0,0 @@
-from .config import *
-from .primitives import *
-from .record import *
--- samples/scripts/__init__.py
+++ samples/scripts/__init__.py
@@ -1,1 +1,12 @@
-from Poppy_Right_Arm import *
+from .config import *
+from .primitives import *
+from .record import *
+
+import pypot.robot
+
+
+class Poppy_Right_Arm(object):
+	"""docstring for Poppy_Right_Arm"""
+	def __init__(self, arg):
+		super(Poppy_Right_Arm, self).__init__()
+		self.right_arm = pypot.robot.config.my_config(config())
